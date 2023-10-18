8709d9c3ba44d74c3b7c0092513fbc9b7aedf55f
Scott McKendry <jerpharney@gmail.com>
2023-10-18 12:35:40 +0200

    fixed ui bug

--- src/Pages/Page_Navs/LandingNav.js
+++ src/Pages/Page_Navs/LandingNav.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function LandingNav() {
-    const [state, setstate] = useState("pokeball_open");
+    const [state, setstate] = useState("pokeball_close");
     const content = () => setstate($state => $state === "pokeball_open" ? "pokeball_close" : "pokeball_open");
 
     return (
