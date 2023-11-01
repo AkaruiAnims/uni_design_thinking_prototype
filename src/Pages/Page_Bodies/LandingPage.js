e51acef69c7fdedadf6333011f4bddb3e6d46bb5
Scott McKendry <jerpharney@gmail.com>
2023-11-01 12:02:30 +0100

    ui fixes

--- src/Pages/Page_Bodies/LandingPage.js
+++ src/Pages/Page_Bodies/LandingPage.js
@@ -7,18 +7,21 @@ function Landingpage( content_array ) {
     return (
     <div className="Landing_Page">
         <div className="Page_Body">
+
             <div className="Page_Content">
                 <div className="Page_Content_Center">
                 </div> 
 
                 <div className="Page_Content_Buttons">
                     <ul>
-                        <li><button onClick={content}>{`${like_state}`} | Like       </button></li>
+                        <li><button onClick={content}>{`${like_state}`} | Like </button></li>
                         <li><button>📓 | Save       </button></li>
                         <li><button>💬 | Comment    </button></li>
                     </ul>
                 </div>
             </div> 
+
+
         </div>
     </div>
     );
